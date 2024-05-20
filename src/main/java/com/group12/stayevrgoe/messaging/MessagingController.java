dfab1e312b8e23db02857bd180637f01b84d6f2a
Anthony <tenmo2003@gmail.com>
2024-05-20 14:19:08 +0700

    feat: added messaging subsystem

--- src/main/java/com/group12/stayevrgoe/messaging/MessagingController.java
+++ src/main/java/com/group12/stayevrgoe/messaging/MessagingController.java
@@ -2,18 +2,28 @@ package com.group12.stayevrgoe.messaging;
 
 import com.group12.stayevrgoe.shared.network.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/message")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class MessagingController {
     private final MessagingService messagingService;
-    @PostMapping
+    @PostMapping("/message")
     public ApiResponse sendMessage(NewMessageDTO dto) {
         return new ApiResponse(HttpStatus.OK, "Message sent", messagingService.sendMessage(dto));
+    }
+
+    @GetMapping("/messages")
+    public ApiResponse getMessages(@RequestParam String chatId,
+                                   @RequestParam(name="page") int pageNumber,
+                                   @RequestParam(name="size") int pageSize) {
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        MessageFilter messageFilter = new MessageFilter(chatId);
+
+        return new ApiResponse(HttpStatus.OK, "OK", messagingService.getMessages(messageFilter, pageable));
     }
 }
--- src/main/java/com/group12/stayevrgoe/messaging/MessagingService.java
+++ src/main/java/com/group12/stayevrgoe/messaging/MessagingService.java
@@ -1,13 +1,35 @@
 package com.group12.stayevrgoe.messaging;
 
+import com.group12.stayevrgoe.shared.utils.AuthenticationUtils;
+import com.group12.stayevrgoe.shared.websocket.WebSocketService;
+import com.nimbusds.jose.shaded.gson.Gson;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MessagingService {
+    private final WebSocketService webSocketService;
+    private final MessageDAO dao;
     public Message sendMessage(NewMessageDTO dto) {
+        Message message = new Message();
+        message.setChatId(dto.getChatId());
+        message.setContent(dto.getContent());
+        message.setSenderEmail(AuthenticationUtils.getCurrentUser().getEmail());
+        message.setType(MessageType.TEXT);
 
-        return null;
+        message = dao.save(message);
+
+        Gson gson = new Gson();
+        webSocketService.send("message", gson.toJson(message));
+
+        return message;
+    }
+
+    public List<Message> getMessages(MessageFilter filter, Pageable pageable) {
+        return dao.get(filter, pageable);
     }
 }
