60e18f55d0ba0f0a6983957a92caa343f44cf7d5
tenmo2003 <tenmo2003@gmail.com>
2024-05-25 21:42:14 +0700

    fix: fix cors

--- src/main/java/com/group12/stayevrgoe/shared/configs/SecurityConfiguration.java
+++ src/main/java/com/group12/stayevrgoe/shared/configs/SecurityConfiguration.java
@@ -43,6 +43,7 @@ public class SecurityConfiguration {
                                 .requestMatchers("/api/users/**").authenticated()
                                 .anyRequest().permitAll())
                 .httpBasic(Customizer.withDefaults())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .exceptionHandling(exception ->
                         exception.authenticationEntryPoint(customAuthenticationEntryPoint)
@@ -61,8 +62,6 @@ public class SecurityConfiguration {
                 "GET", "POST", "PUT", "DELETE", "PATCH"));
         // setAllowCredentials(true) is important, otherwise:
         // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
-        configuration.setAllowCredentials(true);
-        // setAllowedHeaders is important! Without it, OPTIONS preflight request
         // will fail with 403 Invalid CORS request
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
